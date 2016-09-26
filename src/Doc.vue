a90f785d168426ada05d67945fe400cb4643e293
ly <ly@highwe.com>
2016-09-26 10:49:36 +0800

    使用Doc组件改写

--- src/Doc.vue
+++ src/Doc.vue
@@ -1,40 +1,38 @@
+<style>
+</style>
 <template>
-  <div class="ui segment">
-    <div>
-      <h1>Login</h1>
-      <p>
-        登录
-      </p>
-      <table class="ui celled table">
-        <thead>
-          <tr><th>参数</th><th>说明</th></tr>
-        </thead>
-        <tbody>
-          <tr v-for="parm in parms"> <td class="single line"> {{parm.parm}} </td> <td> {{parm.desc}} </td></tr>
-          <tr v-show="parms.length===0">
-            <td colspan="2">这货没有参数</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-    <code v-text="code">
-    </code>
-    <div class="ui divider"></div>
-    <login :login="login" :call_back="call_back"></login>
+  <div>
+    <doc :name="name"
+    :desc="desc"
+    :parm_desc="parm_desc"
+    :parms="parms"
+    :code="code"
+    >
+    <bz :login="login" :call_back="call_back"></bz>
+    </doc>
   </div>
 </template>
 <script>
-  import Login from './Bz'
   import toastr from 'toastr'
+  import 'bz-semantic-ui-card'
+  import 'bz-semantic-ui-grid'
+  import Bz from './Bz'
+  import Doc from 'bz-doc'
   export default {
     components: {
-      Login
+      Bz,
+      Doc
     },
     data: function () {
       return {
+        datas: [1],
+        name: 'bz-login',
+        desc: '登录页面',
         parms: [
+          {parm: 'login', desc: '登录验证等操作函数'},
           {parm: 'call_back', desc: '登录成功后的回调函数'}
         ],
+        parm_desc: `注意，如果使用的组件有路由，那么最好在切换路由的时候发送消息，解除绑定(参看本例子) <code>this.$broadcast('unbind-scroll')</code>`,
         code: `<login :login="login" :call_back="call_back"></login>`
       }
     },
@@ -49,19 +47,3 @@
     }
   }
 </script>
-<style lang=less>
-  .original-text-bz {
-    /*保留空格*/
-    white-space: pre-wrap;
-    /*字体能自动换行*/
-    word-wrap:break-word;
-  }
-  code {
-    padding: 2px 4px;
-    font-size: 90%;
-    color: #c7254e;
-    background-color: #f9f2f4;
-    border-radius: 4px;
-    .original-text-bz;
-  }
-</style>
