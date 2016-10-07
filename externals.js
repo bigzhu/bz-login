da252de641b3ce76d636e6a24089f7078b6455a1
bigzhu <vermiliondun@gmail.com>
2016-10-07 14:37:36 +0800

    有些没必要的 external

--- externals.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  jquery: 'jquery',
-  jQuery: 'jquery',
-  'toastr': 'toastr',
-  'bz-semantic-ui-button': 'bz-semantic-ui-button',
-  'bz-semantic-ui-form': 'bz-semantic-ui-form'
-}
